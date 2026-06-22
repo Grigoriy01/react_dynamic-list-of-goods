6ebebd806413054130b5f2fb68468ceb364037b6
Grigoriy <m9258923028@gmail.com>
2026-06-22 16:08:29 +0200

    solution

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -6,36 +6,31 @@ const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/good
 export function getAll(): Promise<Good[]> {
   return fetch(API_URL)
     .then(response => {
-      if(!response.ok) {
-        throw new Error('Failed to fetch users')
+      if (!response.ok) {
+        throw new Error('Failed to fetch users');
       }
 
-      return response.json()
+      return response.json();
     })
     .then(data => {
-
       return data;
-    })
+    });
 }
 
 // sort and get the first 5
 export const get5First = () => {
-  return getAll()
-    .then(goods => {
-      const sortGoogs  = [...goods].sort((a, b) => a.name.localeCompare(b.name));
+  return getAll().then(goods => {
+    const sortGoogs = [...goods].sort((a, b) => a.name.localeCompare(b.name));
 
-      return sortGoogs.slice(0, 5)
-    });
+    return sortGoogs.slice(0, 5);
+  });
 };
 
 // get only red
 export const getRedGoods = () => {
-  return getAll()
-    .then(goods => {
-      const filterGoogs = goods.filter(el => el.color === 'red');
+  return getAll().then(goods => {
+    const filterGoogs = goods.filter(el => el.color === 'red');
 
-      return filterGoogs;
-    });
+    return filterGoogs;
+  });
 };
-
-
