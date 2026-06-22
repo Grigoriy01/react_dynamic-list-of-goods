c7ad09277c94050b69da826293a221b54929d275
Grigoriy <m9258923028@gmail.com>
2026-06-22 16:07:03 +0200

    solution

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -4,13 +4,38 @@ import { Good } from '../types/Good';
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export function getAll(): Promise<Good[]> {
-  return fetch(API_URL).then(response => response.json());
+  return fetch(API_URL)
+    .then(response => {
+      if(!response.ok) {
+        throw new Error('Failed to fetch users')
+      }
+
+      return response.json()
+    })
+    .then(data => {
+
+      return data;
+    })
 }
 
+// sort and get the first 5
 export const get5First = () => {
-  return getAll().then(goods => goods); // sort and get the first 5
+  return getAll()
+    .then(goods => {
+      const sortGoogs  = [...goods].sort((a, b) => a.name.localeCompare(b.name));
+
+      return sortGoogs.slice(0, 5)
+    });
 };
 
+// get only red
 export const getRedGoods = () => {
-  return getAll().then(goods => goods); // get only red
+  return getAll()
+    .then(goods => {
+      const filterGoogs = goods.filter(el => el.color === 'red');
+
+      return filterGoogs;
+    });
 };
+
+
